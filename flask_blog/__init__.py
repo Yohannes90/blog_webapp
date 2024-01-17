fe66e95cadbca10f60a0a342921c10920a685429
Yohannes90 <mekonnenyohannes90@gmail.com>
2024-01-18 01:11:34 +0300

    working on user authentication, validation, and password encription for registration

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -7,5 +8,6 @@ SECRET_KEY = os.urandom(16)
 app.config['SECRET_KEY'] = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flask_blog import routes
--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_blog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -12,6 +13,16 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                              validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, user_name):
+        user = User.query.filter_by(user_name=user_name.data).first()
+        if user:
+            raise ValidationError('The user name you entered is taken. Please try again with different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('The email you entered is taken. Please try again with different one.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
